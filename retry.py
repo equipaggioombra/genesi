4525e13363a9258cf7d92e77346b7e9f8dd7362b
equipaggioombra <129597671+equipaggioombra@users.noreply.github.com>
2023-04-25 19:52:55 +0000

    Update retry.py

--- retry.py
+++ retry.py
@@ -151,5 +151,5 @@ for item in data:
 # Send notification to telegram
 print("Send notification to telegram")
 url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={TELEGRAM_USERID}&text={message}"
-print(message)
+#print(message)
 print(requests.get(url).json()) # this sends the message
